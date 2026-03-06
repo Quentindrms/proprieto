c8b760c3948b92fc52b8618e30abf6ad31b0de4e
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-06 18:22:38 +0100

    Feats: Enhance styling of PropertyCard component for improved layout and consistency

--- services/frontend/components/propertyCard.tsx
+++ services/frontend/components/propertyCard.tsx
@@ -11,15 +11,15 @@ interface PropertyCardProps {
 
 export default function PropertyCard(props: PropertyCardProps) {
     return (
-        <div class="bg-background-surface w-md p-2">
-            <div id="header" class="border border-background-border flex justify-between items-center" >
+        <div class="bg-background-surface w-md rounded-xl">
+            <div id="header" class="border border-background-border flex justify-between items-center p-2 rounded-xl" >
                 <div class="flex flex-col gap-1 border-b border-background-border">
                     <Text components="p">{props.property.name}</Text>
                     <Badge color="blue">{props.property.type}</Badge>
                 </div>
                 <Badge color={props.property.isActive ? "green" : "red"}>{props.property.isActive ? "Actif" : "Inactif"}</Badge>
             </div >
-            <div id="body" class="flex flex-col gap-2 p-2">
+            <div id="body" class="flex flex-col gap-2 p-2 border-b border-background-border">
                 <div class="flex justify-between">
                     <Text components="p">Prix d'achat</Text>
                     <Text components="p">{props.property.purshacePrice}</Text>
@@ -37,7 +37,7 @@ export default function PropertyCard(props: PropertyCardProps) {
                     <Text components="p">{props.property.sellDate.toLocaleDateString()}</Text>
                 </div>
             </div>
-            <div id="footer" class="flex justify-between">
+            <div id="footer" class="flex justify-between p-2">
                 <Button type="button" color="blue" onClick={props.onEdit}>Modifier</Button>
                 <Button type="button" color="red" onClick={props.onDelete}>Supprimer</Button>
             </div>
