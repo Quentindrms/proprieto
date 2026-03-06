c09268dfe67335b6d8ad9744dcfb9e1a4b12dfe6
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-06 13:45:27 +0100

    Feats: Add style types for text sizes and heading components

---
+++ services/frontend/types/styleTypes.ts
@@ -0,0 +1,12 @@
+import { JSXElement } from "solid-js";
+
+export type TextSize =
+	| "extra-small"
+	| "small"
+	| "base"
+	| "medium"
+	| "large"
+	| "extra-large"
+	| "big";
+
+export type HeadingComponents = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
