9c5806038fcdd4b30ffedad57054f9d34fb8680c
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-06 16:01:29 +0100

    Feats: Add ButtonColor type for enhanced color options in styleTypes

--- services/frontend/types/styleTypes.ts
+++ services/frontend/types/styleTypes.ts
@@ -14,3 +14,5 @@ export type HeadingComponents = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 export type TextComponents = "p" | "span";
 
 export type AccentColor = "green" | "red" | "blue" | "gold";
+
+export type ButtonColor = "green" | "red" | "blue" | "gold";
