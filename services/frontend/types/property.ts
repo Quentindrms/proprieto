151c6b81f02e7f7f9d3f3d090995d2e78e757e79
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-06 18:18:53 +0100

    Feats: Add Property interface for property type definitions

---
+++ services/frontend/types/property.ts
@@ -0,0 +1,10 @@
+export interface Property {
+	name: string;
+	purshacePrice: string;
+	purshaceDate: Date;
+	sellPrice: string;
+	sellDate: Date;
+	isDeleted: boolean;
+	isActive: boolean;
+	type: string;
+}
