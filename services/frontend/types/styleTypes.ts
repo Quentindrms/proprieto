99a6d6acd27300dcd2fb84961632b221104f4c66
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-06 19:14:21 +0100

    Feats: Add 'white' option to HeadingColor type for enhanced color flexibility

--- services/frontend/types/styleTypes.ts
+++ services/frontend/types/styleTypes.ts
@@ -13,6 +13,8 @@ export type HeadingComponents = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 
 export type TextComponents = "p" | "span";
 
+export type HeadingColor = "green" | "red" | "blue" | "gold" | "white";
+
 export type AccentColor = "green" | "red" | "blue" | "gold";
 
 export type ButtonColor = "green" | "red" | "blue" | "gold";
