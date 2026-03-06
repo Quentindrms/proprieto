97e9ac981679a2a35123b38b89fbfc58d25a8ffc
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-06 14:16:28 +0100

    Feats: Create Page component with dynamic Heading for construction notice

---
+++ services/frontend/pages/(landing)/index/+Page.tsx
@@ -0,0 +1,6 @@
+import Heading from "../../../components/heading";
+
+export default function Page() {
+    return (
+        <Heading components="h1" size="extra-large">En cours de construction</Heading>)
+}
