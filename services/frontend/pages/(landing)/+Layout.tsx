7a575640de613a54bd56f59a56baaff20df57379
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-06 14:16:20 +0100

    Feats: Add Layout component for structured page layout

---
+++ services/frontend/pages/(landing)/+Layout.tsx
@@ -0,0 +1,9 @@
+import type { JSX } from "solid-js";
+
+export default function Layout(props: { children?: JSX.Element }) {
+    return (
+        <div class="bg-background-base h-dvh">
+            {props.children}
+        </div>
+    )
+}
