a1b1e4dadb6a93de075f645ec6de8325e6809726
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-06 13:44:42 +0100

    Feats: Implement Heading component with dynamic size classes and props

--- services/frontend/components/heading.tsx
+++ services/frontend/components/heading.tsx
@@ -1,9 +1,31 @@
+import clsx from "clsx";
+import type { JSXElement } from "solid-js";
+import { Dynamic } from "solid-js/web";
+import type { HeadingComponents, TextSize } from "../types/styleTypes";
 
 interface HeadingProps {
-    size: "xs" | "md" | "lg" | ""
+    size: TextSize;
+    components: HeadingComponents;
+    children: JSXElement;
 }
 
-export default function Heading() {
+export default function Heading(props: HeadingProps) {
 
-    return (<></>)
+    const sizeClasses: Record<TextSize, string> = {
+        "extra-small": "text-extra-small",
+        small: "text-small",
+        base: "text-base",
+        medium: "text-medium",
+        large: "text-large",
+        "extra-large": "text-extra-large",
+        big: "text-big",
+    }
+
+    const globalClasses = "font-title"
+
+    return (
+        <Dynamic component={props.components} class={clsx([globalClasses, sizeClasses[props.size]])}>
+            {props.children}
+        </Dynamic>
+    );
 }
