466814cbf389bddbf5ef5d457be649eac09c1a93
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-06 14:16:04 +0100

    Feats: Replace placeholder text with dynamic Heading components for improved structure

--- services/frontend/pages/index/+Page.tsx
+++ services/frontend/pages/index/+Page.tsx
@@ -1,7 +1,14 @@
+import Heading from "../../components/heading";
 
 export default function Page() {
 
     return (
-        <><p class="text-big font-title">Prout le monde</p></>
+        <>
+            <Heading components="h1" size="big">Texte big</Heading>
+            <Heading components="h2" size="extra-large">Texte extra-large</Heading>
+            <Heading components="h3" size="large">Texte large</Heading>
+            <Heading components="h4" size="medium">Texte medium</Heading>
+            <Heading components="h5" size="small">Texte small</Heading>
+        </>
     )
 }
