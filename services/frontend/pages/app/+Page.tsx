9318d608d1609e8a8a8318bf01fe93dc1ad6b8fa
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-06 14:16:37 +0100

    Feats: Create Page component with dynamic Heading for application welcome

---
+++ services/frontend/pages/app/+Page.tsx
@@ -0,0 +1,11 @@
+import Heading from "../../components/heading";
+
+export default function Page() {
+
+    return (
+        <div>
+            <Heading components="h1" size="extra-large">
+                Bienvenue dans l'application
+            </Heading>
+        </div>)
+}
