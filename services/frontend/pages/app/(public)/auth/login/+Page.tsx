b55849f1a569a47817c0d922ef7f3f20149772bf
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-09 15:14:52 +0100

    Feats: Update Login page layout to use a div wrapper for improved styling

--- services/frontend/pages/app/(public)/auth/login/+Page.tsx
+++ services/frontend/pages/app/(public)/auth/login/+Page.tsx
@@ -3,8 +3,8 @@ import LoginForm from "./form";
 export default function Page() {
 
     return (
-        <>
+        <div class="h-dvh flex flex-col justify-center items-center">
             <LoginForm />
-        </>
+        </div>
     )
 }
