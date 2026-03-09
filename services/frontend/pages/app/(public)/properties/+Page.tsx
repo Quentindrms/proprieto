2f0c0328db1aab78fd7c1883f17f44a28f9e5c5f
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-09 16:52:14 +0100

    Feats: Refactor Page component to include StatCard, SearchField, and PropertyCard for enhanced property management

--- services/frontend/pages/app/(public)/properties/+Page.tsx
+++ services/frontend/pages/app/(public)/properties/+Page.tsx
@@ -1,8 +1,73 @@
+import { Button } from "../../../../components/button";
+import { StatCard } from "../../../../components/cards";
+import Heading from "../../../../components/heading";
+import PropertyCard from "../../../../components/propertyCard";
+import SearchField from "../../../../components/searchField";
+import type { Property } from "../../../../types/property";
+
 export default function Page() {
+    const property: Property = {
+        name: "Ma propriété",
+        purshacePrice: "100 000",
+        purshaceDate: new Date(),
+        sellPrice: "0",
+        sellDate: new Date(),
+        isDeleted: false,
+        isActive: true,
+        type: "Appartement",
+    };
 
     return (
-        <>
+        <div>
+            <div class="flex justify-between p-2">
+                <Heading components="h1" size="large" color="white">Mes propriétés</Heading>
+                <Button type="button">Créer une nouvelle propriété</Button>
+            </div>
+            <div
+                id="top-wrapper"
+                class="flex p-2 gap-5 justify-between">
+                <StatCard
+                    title=""
+                    value="0"
+                    legend="Propriétés au total"
+                />
+                <StatCard
+                    title=""
+                    value="0"
+                    legend="Actives"
+                />
+                <StatCard
+                    title=""
+                    value="0"
+                    legend="En location"
+                />
+                <StatCard
+                    title=""
+                    value="0"
+                    legend="Vendues"
+                />
+            </div>
+            <div class="p-4">
+                <SearchField name="searchfield" placeholder="Recherchez..." />
+            </div>
 
-        </>
+            <div class="p-4 grid grid-cols-3 gap-4">
+                <PropertyCard
+                    property={property}
+                />
+                <PropertyCard
+                    property={property}
+                />
+                <PropertyCard
+                    property={property}
+                />
+                <PropertyCard
+                    property={property}
+                />
+                <PropertyCard
+                    property={property}
+                />
+            </div>
+        </div>
     )
 }
