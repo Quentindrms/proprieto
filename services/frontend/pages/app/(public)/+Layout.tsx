13f8a618275f8ef031a0380233c09d748cced727
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-09 17:06:25 +0100

    Feats: Remove height utility from Layout component for improved flexibility

--- services/frontend/pages/app/(public)/+Layout.tsx
+++ services/frontend/pages/app/(public)/+Layout.tsx
@@ -2,7 +2,7 @@ import type { JSX } from "solid-js";
 import Navbar from "../../../components/navbar";
 
 export default function Layout(props: { children?: JSX.Element }) {
-    return <div class="flex bg-background-base h-dvh gap-2">
+    return <div class="flex bg-background-base gap-2">
         <Navbar />
         {props.children}
     </div>;
