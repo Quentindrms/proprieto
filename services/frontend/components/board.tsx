4edb4646aa2ab85b81ad8f880768d77fd478a482
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-09 09:05:38 +0100

    Feats: Implement Board and ContractBoardLine components for enhanced layout and functionality

---
+++ services/frontend/components/board.tsx
@@ -0,0 +1,68 @@
+import { For, type JSX } from "solid-js";
+import type { BadgeColor } from "../types/styleTypes";
+import { Badge } from "./badge";
+import Heading from "./heading";
+import SearchField from "./searchField";
+import Text from "./text";
+
+export interface BoardProps {
+    name: string;
+    children: JSX.Element;
+    columns: string[];
+}
+
+export default function Board(props: BoardProps) {
+    return (
+        <div class="flex flex-col w-3xl bg-background-surface border border-background-border">
+            <div
+                id="header"
+                class="flex justify-between border-b border-background-border p-2"
+            >
+                <Heading components="h3" size="medium">
+                    {props.name}
+                </Heading>
+                <SearchField name="Recherche" />
+            </div>
+            <div id="body" class="p-2">
+                <table class="w-full">
+                    <thead>
+                        <tr class="border-b border-background-border w-full">
+                            <For each={props.columns}>
+                                {(column) => (
+                                    <th class="text-left p-4">
+                                        <Text components="p">{column}</Text>
+                                    </th>
+                                )}
+                            </For>
+                        </tr>
+                    </thead>
+                    <tbody>{props.children}</tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
+
+interface ContractBoardLineProps {
+    cells: string[];
+    status: { label: string; color: BadgeColor };
+    onEdit?: () => void;
+    onDelete?: () => void;
+}
+
+export function ContractBoardLine(props: ContractBoardLineProps) {
+    return (
+        <tr class="border-b border-backgroiund-border last:border-0">
+            <For each={props.cells}>
+                {(cell) => (
+                    <td class="p-4">
+                        <Text components="p">{cell}</Text>
+                    </td>
+                )}
+            </For>
+            <td class="p-4">
+                <Badge color={props.status.color}>{props.status.label}</Badge>
+            </td>
+        </tr>
+    );
+}
