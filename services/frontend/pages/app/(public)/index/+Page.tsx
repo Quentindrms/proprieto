a1da73181d3e5136a215e7a4a8c88843da3cbb6a
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-09 15:22:40 +0100

    Feats: Create initial Page component for Dashboard with Heading

---
+++ services/frontend/pages/app/(public)/index/+Page.tsx
@@ -0,0 +1,10 @@
+import Heading from "../../../../components/heading";
+
+export default function Page() {
+
+    return (
+        <div>
+            <Heading components="h1" size="big">Hello world - Dashboard</Heading>
+        </div>
+    )
+}
