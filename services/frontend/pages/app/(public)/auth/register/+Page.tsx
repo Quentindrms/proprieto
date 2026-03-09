5f29b35f735427097a56d6529c3d3d0a628515ed
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-09 15:15:13 +0100

    Feats: Update Register page layout to use a div wrapper for improved styling

--- services/frontend/pages/app/(public)/auth/register/+Page.tsx
+++ services/frontend/pages/app/(public)/auth/register/+Page.tsx
@@ -3,8 +3,8 @@ import RegisterForm from "./form";
 export default function Page() {
 
     return (
-        <>
+        <div class="h-dvh flex justify-center items-center">
             <RegisterForm />
-        </>
+        </div>
     )
 }
