0e8e457d586516afc8de1b8493fb566a0c5fe636
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-09 16:18:01 +0100

    Feats: Update Page component with localized greeting and date display

--- services/frontend/pages/app/(public)/index/+Page.tsx
+++ services/frontend/pages/app/(public)/index/+Page.tsx
@@ -1,10 +1,49 @@
+import { StatCard } from "../../../../components/cards";
 import Heading from "../../../../components/heading";
+import Text from "../../../../components/text";
 
 export default function Page() {
 
+    const date = new Date();
+
     return (
         <div>
-            <Heading components="h1" size="big">Hello world - Dashboard</Heading>
+            <Heading components="h1" size="big" color="white">Bonjour, username</Heading>
+
+            <Text components="p" size="base">{date.getDate()} / {date.getMonth() + 1} / {date.getFullYear()} - Vue d'ensemble</Text>
+
+            <div id="top-wrapper"
+                class="flex gap-5 border"
+            >
+                <StatCard
+                    legend="Deuis hier"
+                    value="0"
+                    accentColor="blue"
+                    title="Titre"
+                />
+
+                <StatCard
+                    legend="Deuis hier"
+                    value="0"
+                    accentColor="blue"
+                    title="Titre"
+                />
+
+                <StatCard
+                    legend="Deuis hier"
+                    value="0"
+                    accentColor="blue"
+                    title="Titre"
+                />
+
+                <StatCard
+                    legend="Deuis hier"
+                    value="0"
+                    accentColor="blue"
+                    title="Titre"
+                />
+            </div>
+
         </div>
     )
 }
