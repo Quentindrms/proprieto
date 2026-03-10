a6bc8ccff6e8d0544264b04a4f93d8993e0a37fc
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-10 09:52:23 +0100

    Feats: Add Navbar component for improved navigation structure

---
+++ services/frontend/components/navbar.tsx
@@ -0,0 +1,47 @@
+import Heading from "./heading";
+import Text from "./text";
+
+export default function Navbar() {
+
+    return (
+        <div class="h-full w-xs shrink-0 p-2 flex flex-col border-r border-background-border">
+            <div class="text-center p-3">
+                <Heading components="h1" size="big"><a href="/app">Proprieto</a></Heading>
+                <Text components="p">Gestion immobilière</Text>
+            </div>
+
+            <div class="flex flex-col gap-2">
+                <Heading components="h2" size="large" color="white">Vue générale</Heading>
+                <NavbarLink name="Tableau de bord" value="/app/" />
+
+                <Heading components="h2" size="large" color="white">Patrimoine</Heading>
+                <NavbarLink name="Propriétés" value="/app/properties" />
+                <NavbarLink name="Contrats" value="/app/contracts" />
+
+                <Heading components="h2" size="large" color="white">Finances</Heading>
+                <NavbarLink name="Revenus" value="/app/incomes" />
+                <NavbarLink name="Dépense" value="/app/outcomes" />
+
+                <Heading components="h2" size="large" color="white">Contacts</Heading>
+                <NavbarLink name="Clients" value="/app/clients" />
+                <NavbarLink name="Prestataires" value="/app/contractors" />
+
+                <Heading components="h2" size="large" color="white">Compte</Heading>
+                <NavbarLink name="Paramètres" value="/app/account" />
+            </div>
+        </div>
+    )
+}
+
+interface NavbarLinkProps {
+    name: string,
+    value: string,
+}
+
+export function NavbarLink(props: NavbarLinkProps) {
+
+    return (
+        <a href={props.value} class="text-primary hover:text-solid-gold-hover text-medium pl-2">
+            {props.name}</a>
+    )
+}
