746748519756fa261315a06d0cc536588d70bbae
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-10 09:53:07 +0100

    Feats: Remove Navbar and associated components for layout simplification

--- services/frontend/pages/app/(public)/+Layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { JSX } from "solid-js";
-import Navbar from "../../../components/navbar";
-
-export default function Layout(props: { children?: JSX.Element }) {
-    return <div class="flex bg-background-base gap-2">
-        <Navbar />
-        {props.children}
-    </div>;
-}
