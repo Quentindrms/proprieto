9c8feb5744b18287ca5757d74cf9ae886ec2d43a
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-10 09:52:50 +0100

    Feats: Add PageNamer component for dynamic page title and action button

---
+++ services/frontend/components/pageNamer.tsx
@@ -0,0 +1,21 @@
+import { Button } from "./button";
+import Heading from "./heading";
+
+interface PageNamerProps {
+	pageName: string;
+	buttonText: string;
+	onClick: () => void;
+}
+
+export default function PageNamer(props: PageNamerProps) {
+	return (
+		<div class="flex justify-between">
+			<Heading components="h1" size="large">
+				{props.pageName}
+			</Heading>
+			<Button type="button" onClick={props.onClick}>
+				{props.buttonText}
+			</Button>
+		</div>
+	);
+}
