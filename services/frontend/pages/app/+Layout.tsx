232ca22df70010b6f9079d1a5d180d93b63f0270
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-10 09:52:57 +0100

    Feats: Update Layout component to include Navbar for improved navigation

--- services/frontend/pages/app/+Layout.tsx
+++ services/frontend/pages/app/+Layout.tsx
@@ -1,5 +1,11 @@
 import type { JSX } from "solid-js";
+import Navbar from "../../components/navbar";
 
 export default function Layout(props: { children?: JSX.Element }) {
-    return <div class="bg-background-base h-dvh">{props.children}</div>;
+	return (
+		<div class="bg-background-base flex">
+			<Navbar />
+			{props.children}
+		</div>
+	);
 }
