2c4278c4e7bec6ae39946690e109e183c912d9b7
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-10 09:52:32 +0100

    Feats: Implement Page component layout with PageNamer and StatCards for contract overview

--- services/frontend/pages/app/(public)/contracts/+Page.tsx
+++ services/frontend/pages/app/(public)/contracts/+Page.tsx
@@ -1,8 +1,22 @@
+import { StatCard } from "../../../../components/cards";
+import Heading from "../../../../components/heading";
+import PageNamer from "../../../../components/pageNamer";
 
 export default function Page() {
-
-    return (
-        <>
-        </>
-    )
+	return (
+		<div class="w-dvw h-dvh">
+			<PageNamer
+				pageName="Mes contrats"
+				buttonText="Ajouter un contrat"
+				onClick={() => {}}
+			/>
+			<div id="top-wrapper" class="flex w-full gap-5">
+				<StatCard legend="Légend" value="0" title="Titre" accentColor="blue" />
+				<StatCard legend="Légend" value="0" title="Titre" accentColor="blue" />
+				<StatCard legend="Légend" value="0" title="Titre" accentColor="blue" />
+				<StatCard legend="Légend" value="0" title="Titre" accentColor="blue" />
+			</div>
+			<div></div>
+		</div>
+	);
 }
