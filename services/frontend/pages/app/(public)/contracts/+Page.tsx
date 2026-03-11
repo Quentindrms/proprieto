869300088b2a69b5277bec2269a05137c949796a
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-11 10:21:41 +0100

    Refactor: Refactor Page component to include SearchField and Board for enhanced functionality

--- services/frontend/pages/app/(public)/contracts/+Page.tsx
+++ services/frontend/pages/app/(public)/contracts/+Page.tsx
@@ -1,6 +1,9 @@
-import { StatCard } from "../../../../components/cards";
+import Board from "../../../../components/board";
+import { StatCard, StatCardWrapper } from "../../../../components/cards";
 import Heading from "../../../../components/heading";
 import PageNamer from "../../../../components/pageNamer";
+import SearchField from "../../../../components/searchField";
+import Text from "../../../../components/text";
 
 export default function Page() {
 	return (
@@ -10,13 +13,18 @@ export default function Page() {
 				buttonText="Ajouter un contrat"
 				onClick={() => {}}
 			/>
-			<div id="top-wrapper" class="flex w-full gap-5">
+			<StatCardWrapper>
 				<StatCard legend="Légend" value="0" title="Titre" accentColor="blue" />
 				<StatCard legend="Légend" value="0" title="Titre" accentColor="blue" />
 				<StatCard legend="Légend" value="0" title="Titre" accentColor="blue" />
 				<StatCard legend="Légend" value="0" title="Titre" accentColor="blue" />
+			</StatCardWrapper>
+			<div class="p-2">
+				<SearchField name="searchbar" placeholder="Effectuer une recherche" />
 			</div>
-			<div></div>
+			<Board columns={[]} name="Contrats">
+				<Text components="p">Test</Text>
+			</Board>
 		</div>
 	);
 }
