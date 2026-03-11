0656c8ea8de4501aaa2b30b56b6d777473a3122b
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-11 10:46:33 +0100

    Feats: Implement Page component layout with StatCard, SearchField, and Board for expense overview

--- services/frontend/pages/app/(public)/outcomes/+Page.tsx
+++ services/frontend/pages/app/(public)/outcomes/+Page.tsx
@@ -1,5 +1,36 @@
+import Board from "../../../../components/board";
+import {
+	BasicCard,
+	StatCard,
+	StatCardWrapper,
+} from "../../../../components/cards";
+import PageNamer from "../../../../components/pageNamer";
+import SearchField from "../../../../components/searchField";
+import Text from "../../../../components/text";
 
 export default function Page() {
-
-    return (<></>)
+	return (
+		<div class="h-dvh w-dvw flex flex-col">
+			<PageNamer
+				pageName="Mes dépenses"
+				buttonText="Ajouter une dépense"
+				onClick={() => {}}
+			/>
+			<StatCardWrapper>
+				<StatCard legend="" value="0" accentColor="blue" title="Ce mois" />
+				<StatCard legend="" value="0" accentColor="blue" title="Cette année" />
+				<StatCard legend="" value="0" accentColor="blue" title="Récurrentes" />
+				<StatCard legend="" value="0" accentColor="blue" title="Non payées" />
+			</StatCardWrapper>
+			<div class="p-2">
+				<SearchField name="searchbar" placeholder="Effectuer une recherche" />
+			</div>
+			<div class="p-5 flex justify-around">
+				<Board columns={[]} name="Contrats">
+					<Text components="p">Test</Text>
+				</Board>
+				<BasicCard title="Dépenses par catégorie" />
+			</div>
+		</div>
+	);
 }
