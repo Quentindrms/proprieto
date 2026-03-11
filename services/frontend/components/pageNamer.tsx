f83aee7c892239e9188b8648b0e783db1be85b43
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-11 12:08:14 +0100

    Feats: Add PageNamerDater component for enhanced user greeting and date display

--- services/frontend/components/pageNamer.tsx
+++ services/frontend/components/pageNamer.tsx
@@ -1,6 +1,7 @@
 import type { ButtonIcons } from "../types/styleTypes";
 import { Button } from "./button";
 import Heading from "./heading";
+import Text from "./text";
 
 interface PageNamerProps {
 	pageName: string;
@@ -15,6 +16,37 @@ export default function PageNamer(props: PageNamerProps) {
 			<Heading components="h1" size="large">
 				{props.pageName}
 			</Heading>
+			<Button type="button" onClick={props.onClick} icons={props.buttonIcons}>
+				{props.buttonText}
+			</Button>
+		</div>
+	);
+}
+
+interface PageNamerDaterProps {
+	buttonText: string;
+	username: string;
+	onClick: () => void;
+	buttonIcons?: ButtonIcons;
+}
+
+export function PageNamerDater(props: PageNamerDaterProps) {
+	const date = new Date().toLocaleDateString("fr-FR", {
+		day: "2-digit",
+		month: "long",
+		year: "numeric",
+		hour: "numeric",
+		hourCycle: "h24",
+	});
+
+	return (
+		<div class="flex justify-between p-4 items-center">
+			<div class="flex flex-col gap-2">
+				<Heading components="h1" size="large">
+					Bonjour, {props.username}
+				</Heading>
+				<Text>{date}</Text> - Vue d'ensemble
+			</div>
 			<Button type="button" onClick={props.onClick} icons={props.buttonIcons}>
 				{props.buttonText}
 			</Button>
