efe892e26272dd149d9ef2ef2286ef121b8d1de3
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-11 11:20:54 +0100

    Feats: Enhance Page component layout with StatCards and improved structure

--- services/frontend/pages/app/(public)/contractors/+Page.tsx
+++ services/frontend/pages/app/(public)/contractors/+Page.tsx
@@ -1,9 +1,30 @@
+import Board from "../../../../components/board";
+import { StatCard, StatCardWrapper } from "../../../../components/cards";
+import ClientCard from "../../../../components/clientCard";
+import PageNamer from "../../../../components/pageNamer";
+import Text from "../../../../components/text";
 
 export default function Page() {
+	return (
+		<div class="w-dvw h-dvh">
+			<PageNamer
+				pageName="Mes clients"
+				buttonText="Ajouter un client"
+				onClick={() => {}}
+			/>
+			<StatCardWrapper>
+				<StatCard legend="Clients totaux" value="0" title="" />
 
-    return (
-        <>
+				<StatCard legend="Avec contrat actif" value="0" title="" />
 
-        </>
-    )
+				<StatCard legend="Sans contrat" value="0" title="" />
+			</StatCardWrapper>
+
+			<div class="p-5 grid [grid-template-columns:max-content_max-content_max-content] gap-x-5 gap-y-5">
+				<Board name="Pestataires" columns={[]}>
+					<Text components="p">Test</Text>
+				</Board>
+			</div>
+		</div>
+	);
 }
