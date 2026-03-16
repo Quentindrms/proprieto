3a3b7eef671f2b6c93be4f0c19eab494f9c3a3d1
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-16 10:02:24 +0100

    Feats: Simplify layout classes in Page component for improved responsiveness

--- services/frontend/pages/app/(public)/clients/+Page.tsx
+++ services/frontend/pages/app/(public)/clients/+Page.tsx
@@ -4,11 +4,11 @@ import PageNamer from "../../../../components/pageNamer";
 
 export default function Page() {
 	return (
-		<div class="w-dvw h-dvh">
+		<div class="">
 			<PageNamer
 				pageName="Mes clients"
 				buttonText="Ajouter un client"
-				onClick={() => {}}
+				onClick={() => { }}
 			/>
 			<StatCardWrapper>
 				<StatCard legend="Clients totaux" value="0" title="" />
@@ -18,7 +18,7 @@ export default function Page() {
 				<StatCard legend="Sans contrat" value="0" title="" />
 			</StatCardWrapper>
 
-			<div class="p-5 grid [grid-template-columns:max-content_max-content_max-content] gap-x-5 gap-y-5">
+			<div class="grid grid-cols-3 gap-2">
 				<ClientCard
 					client={{
 						adress: "13 rue des écoles 69000 LYON",
