30bea05465347d6a400a915ef9cf15bf53948163
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-16 10:14:26 +0100

    Feats: Remove width and height classes from Page component for improved layout flexibility

--- services/frontend/pages/app/(public)/properties/+Page.tsx
+++ services/frontend/pages/app/(public)/properties/+Page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
 	};
 
 	return (
-		<div class="w-dvw h-dvh">
+		<div class="">
 			<div class="flex justify-between p-2">
 				<Heading components="h1" size="large" color="white">
 					Mes propriétés
