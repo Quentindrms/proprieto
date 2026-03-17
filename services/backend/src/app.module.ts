6fa3d2a6c2aa44ead4a5091ffd8b2832c8063cd5
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-17 14:17:24 +0100

    Feats(app): Initialize AppModule with necessary imports for modules

--- services/backend/src/app.module.ts
+++ services/backend/src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { ClientModule } from "./client/client.module";
+import { ContractModule } from "./contract/contract.module";
+import { IncomeModule } from "./income/income.module";
+import { OutcomeModule } from "./outcome/outcome.module";
+import { PropertyModule } from "./property/property.module";
+import { ProviderModule } from "./provider/provider.module";
+import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		AuthModule,
+		ClientModule,
+		ContractModule,
+		IncomeModule,
+		OutcomeModule,
+		PropertyModule,
+		ProviderModule,
+		UserModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
