8503e04747480763ca4c8ced5ad64f57611627ab
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-17 16:13:15 +0100

    Fix(database): Correct typo in environment variable name for database connection

--- services/backend/libs/DatabaseClient.ts
+++ services/backend/libs/DatabaseClient.ts
@@ -6,7 +6,7 @@ class DatabaseClient {
 	private static instance: PrismaClient;
 
 	private constructor() {
-		const connectionString = process.env.DATABSE_URL;
+		const connectionString = process.env.DATABASE_URL;
 		if (!connectionString) throw new Error("DATABASE_URL is required");
 
 		const adapter = new PrismaPg({ connectionString });
