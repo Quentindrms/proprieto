0769520a4cb23c9b92fe0abf9df24f38bc50ab08
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-17 16:18:28 +0100

    Feat(auth): Implement user registration and login functionality

--- services/backend/src/auth/auth.controller.ts
+++ services/backend/src/auth/auth.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Get } from "@nestjs/common";
-import type { AuthService } from "./auth.service";
+import { Body, Controller, Get, Post, Res } from "@nestjs/common";
+import type { Response } from "express";
+import type { CreateAccountDto } from "types/DtoType";
+//biome-ignore lint/style/useImportType: required for NestJS DI
+import { AuthService } from "./auth.service";
 
 @Controller("auth")
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
-	@Get()
-	test() {}
+	@Post("login")
+	async login() {}
+
+	@Post("register")
+	async register(@Body() body: CreateAccountDto, @Res() response: Response) {
+		const registeredUser = this.authService.register(body);
+		if (!registeredUser)
+			return response.status(400).send({ message: "An error as occured" });
+		return response.status(200).send({ message: "Account created" });
+	}
 }
--- services/backend/src/auth/auth.service.ts
+++ services/backend/src/auth/auth.service.ts
@@ -1,4 +1,20 @@
+import { prisma } from "@libs/DatabaseClient";
 import { Injectable } from "@nestjs/common";
+import type { CreateAccountDto } from "types/DtoType";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	async register(account: CreateAccountDto) {
+		const argon2 = require("argon2");
+		const user = await prisma.user.create({
+			data: {
+				name: account.name,
+				firstName: account.firstName,
+				address: account.address,
+				email: account.email,
+				phone: account.phone,
+				password: await argon2.hash(account.password),
+			},
+		});
+	}
+}
