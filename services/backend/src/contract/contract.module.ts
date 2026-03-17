f82fe166b7b063e4e998ff9c6cc95e2e1fbbae6c
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-17 14:01:36 +0100

    Feats(contract): Implement Contract module with ContractController and ContractService

---
+++ services/backend/src/contract/contract.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { ContractController } from "./contract.controller";
+import { ContractService } from "./contract.service";
+
+@Module({
+	imports: [],
+	controllers: [ContractController],
+	providers: [ContractService],
+})
+export class ContractModule {}
