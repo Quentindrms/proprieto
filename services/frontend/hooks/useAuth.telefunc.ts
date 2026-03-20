aa6f91673cae4dbcda26727c4e4b8397d8d1c0a8
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-20 11:24:39 +0100

    feat(auth): Enhance login function to set auth cookie upon successful login

--- services/frontend/hooks/useAuth.telefunc.ts
+++ services/frontend/hooks/useAuth.telefunc.ts
@@ -1,9 +1,16 @@
 import { AuthService } from "@services/auth.service";
+import { setAuthCookie, setCookie } from "@utils/cookie";
+import { getContext } from "@utils/telefunc";
 
 export async function onLogin(email: string, password: string) {
+	const context = getContext();
+
 	try {
 		const authService = new AuthService();
-		authService.login(email, password);
+		const response = await authService.login(email, password);
+		if (response?.token) {
+			setAuthCookie(context.fastify.reply, response.token);
+		}
 	} catch (error) {
 		console.trace(error);
 	}
