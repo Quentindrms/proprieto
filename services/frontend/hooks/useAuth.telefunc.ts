8c0f453c1c6979c5963db0273f4c006650ef6d7a
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-20 10:34:22 +0100

    Fix(auth): Ensure error is logged in onLogin function

--- services/frontend/hooks/useAuth.telefunc.ts
+++ services/frontend/hooks/useAuth.telefunc.ts
@@ -3,6 +3,8 @@ import { AuthService } from "@services/auth.service";
 export async function onLogin(email: string, password: string) {
 	try {
 		const authService = new AuthService();
-		const response = authService.login(email, password);
-	} catch (error) {}
+		authService.login(email, password);
+	} catch (error) {
+		console.trace(error);
+	}
 }
