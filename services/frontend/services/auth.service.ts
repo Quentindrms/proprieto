527ac66d76aa10ff9c57d2043d1fadea648054f4
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-20 11:24:45 +0100

    feat(auth): Update login method to return response with success status and token

--- services/frontend/services/auth.service.ts
+++ services/frontend/services/auth.service.ts
@@ -4,7 +4,10 @@ import { CoreService } from "./core.service";
 export class AuthService extends CoreService {
 	async login(email: string, password: string) {
 		try {
-			this.post("/auth/login", { email, password });
+			return this.post<{ success: boolean; token: string }>("/auth/login", {
+				email,
+				password,
+			});
 		} catch (error) {
 			console.trace(error);
 		}
