9f68b49a38c2ecc5204f9c97f41cf2cdd2112a68
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-20 15:14:12 +0100

    feat(auth): Implement IsAuthenticatedMiddleware for JWT verification

---
+++ services/backend/middleware/is-authenticated.middleware.ts
@@ -0,0 +1,28 @@
+import { prisma } from "@libs/DatabaseClient";
+import { Injectable, type NestMiddleware } from "@nestjs/common";
+import type { NextFunction, Request, Response } from "express";
+import { JwtService } from "services/jwt.service";
+
+@Injectable()
+export class IsAuthenticatedMiddleware
+	extends JwtService
+	implements NestMiddleware
+{
+	async use(request: Request, response: Response, next: NextFunction) {
+		const bearerToken = request.headers.authorization?.split(" ")[1];
+		if (typeof bearerToken !== "string" || bearerToken === "null") {
+			return response.status(401).send();
+		}
+
+		const { userId } = await this.verifyJWT(bearerToken);
+		if (typeof userId !== "string") return response.status(401).send();
+
+		const user = await prisma.user.findFirst({
+			where: { id: userId },
+		});
+		if (!user) return response.status(401).send();
+
+		request.user = user;
+		next();
+	}
+}
