40bb09368c6a560ab840b077d27356b4c6785c43
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-20 11:05:32 +0100

    feat(auth): Extend global PageContext to include user and Fastify types

--- services/frontend/server/entry.ts
+++ services/frontend/server/entry.ts
@@ -1,4 +1,6 @@
+import type { User } from "@app/types/user";
 import { apply, serve } from "@photonjs/fastify";
+import type { FastifyReply, FastifyRequest } from "fastify";
 import fastify from "fastify";
 import rawBody from "fastify-raw-body";
 import { telefuncHandler } from "./telefunc-handler";
@@ -6,6 +8,21 @@ import { telefuncHandler } from "./telefunc-handler";
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 export default (await startApp()) as unknown;
+
+declare global {
+	namespace Vike {
+		interface PageContext {
+			//Authentification
+			user?: User;
+
+			//Server data
+			fastify: {
+				request: FastifyRequest;
+				reply: FastifyReply;
+			};
+		}
+	}
+}
 
 async function startApp() {
 	const app = fastify({
