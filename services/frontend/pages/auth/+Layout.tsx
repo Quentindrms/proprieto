bf022f8cdacaecc6f513859f4263abb1864a575d
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-20 16:50:51 +0100

    feat(auth): Add Layout component for authentication pages

--- services/frontend/pages/auth/+Layout.tsx
+++ services/frontend/pages/auth/+Layout.tsx
@@ -1,10 +1,11 @@
 import Navbar from "@components/navbar";
 import type { JSX } from "solid-js";
+import { Toaster } from "solid-toast";
 
 export default function Layout(props: { children?: JSX.Element }) {
 	return (
 		<div class="min-h-dvh bg-background-base flex">
-			<Navbar />
+			<Toaster position="top-right" />
 			<main class="flex min-w-0">{props.children}</main>
 		</div>
 	);
