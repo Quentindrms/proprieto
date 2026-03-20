570aa469e7f6f4bc65285a3dd2f91a268cee986f
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-20 14:07:30 +0100

    feat(auth): Add registration handling and enhance login flow with navigation

--- services/frontend/hooks/useAuth.ts
+++ services/frontend/hooks/useAuth.ts
@@ -1,10 +1,15 @@
 import { createSignal } from "solid-js";
 import toast from "solid-toast";
+import { navigate } from "vike/client/router";
 import { onLogin } from "./useAuth.telefunc";
 
 export function useAuth() {
 	const [email, setEmail] = createSignal<string>("");
 	const [password, setPassword] = createSignal<string>("");
+	const [passwordValidation, setPasswordValidation] = createSignal<string>("");
+	const [name, setName] = createSignal<string>("");
+	const [firstName, setFirstName] = createSignal<string>("");
+	const [phone, setPhone] = createSignal<string>("");
 
 	async function handleLogin(event: SubmitEvent) {
 		try {
@@ -12,6 +17,7 @@ export function useAuth() {
 			const response = await onLogin(email(), password());
 			if (response?.success) {
 				toast.success("Connexion réussie");
+				navigate("/app/");
 			} else {
 				toast.error("Une erreur est survenue lors de la connexion");
 			}
@@ -20,11 +26,24 @@ export function useAuth() {
 		}
 	}
 
+	async function handleRegister(event: SubmitEvent) {
+		event.preventDefault();
+	}
+
 	return {
 		setEmail,
 		email,
 		setPassword,
 		password,
+		setFirstName,
+		firstName,
+		setName,
+		name,
+		setPhone,
+		phone,
+		setPasswordValidation,
+		passwordValidation,
 		handleLogin,
+		handleRegister,
 	};
 }
