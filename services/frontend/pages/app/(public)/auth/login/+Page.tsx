adfcd41aef9018a07cd0aa9021aec05080381753
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-20 09:41:03 +0100

    Feat(auth): Enhance login form with state management and input handling

--- services/frontend/pages/app/(public)/auth/login/+Page.tsx
+++ services/frontend/pages/app/(public)/auth/login/+Page.tsx
@@ -1,10 +1,13 @@
+import Heading from "@components/heading";
 import LoginForm from "./form";
 
 export default function Page() {
-
-    return (
-        <div class="h-dvh flex flex-col justify-center items-center">
-            <LoginForm />
-        </div>
-    )
+	return (
+		<div class="flex flex-col justify-center items-center w-dvw">
+			<Heading components="h1" size="big" color="white">
+				Connexion
+			</Heading>
+			<LoginForm />
+		</div>
+	);
 }
--- services/frontend/pages/app/(public)/auth/login/form.tsx
+++ services/frontend/pages/app/(public)/auth/login/form.tsx
@@ -1,15 +1,45 @@
+import { createSignal } from "solid-js";
 import { Button } from "../../../../../components/button";
 import { TextField } from "../../../../../components/form";
 
-export default function LoginForm() {
+interface LoginFormField {
+	email: string;
+	password: string;
+}
 
-    return (
-        <div class="flex flex-col gap-3 p-2 w-lg bg-background-surface border border-background-border rounded-xl shadow-md shadow-background-border">
-            <TextField label="Adrese email" type="email" />
-            <TextField label="Mot de passe" type="password" />
-            <div class="flex justify-center">
-                <Button type="submit">Connexion</Button>
-            </div>
-        </div>
-    )
+export default function LoginForm() {
+	const [formData, setFormData] = createSignal<LoginFormField>({
+		email: "",
+		password: "",
+	});
+
+	function handleInputChange(field: keyof LoginFormField) {
+		return (event: InputEvent) => {
+			const target = event.target as HTMLInputElement;
+			setFormData((prev) => ({
+				...prev,
+				[field]: target.value,
+			}));
+		};
+	}
+
+	return (
+		<div class="flex flex-col gap-3 p-2 w-lg bg-background-surface border border-background-border rounded-xl shadow-md shadow-background-border">
+			<TextField
+				label="Adrese email"
+				type="email"
+				name="mail"
+				onInput={handleInputChange("email")}
+			/>
+			<TextField
+				label="Mot de passe"
+				type="password"
+				name="password"
+				onInput={handleInputChange("password")}
+			/>
+			<div class="flex justify-center">
+				<Button type="submit">Connexion</Button>
+			</div>
+		</div>
+	);
 }
