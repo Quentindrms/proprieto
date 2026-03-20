b2093d21cc511fccba872b68ec69e74a8391050b
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-20 10:24:58 +0100

    Fix(auth): Remove unused result variable in login method of AuthService

--- services/frontend/services/auth.service.ts
+++ services/frontend/services/auth.service.ts
@@ -3,7 +3,7 @@ import { CoreService } from "./core.service";
 export class AuthService extends CoreService {
 	async login(email: string, password: string) {
 		try {
-			const result = this.post("/auth/login", { email, password });
+			this.post("/auth/login", { email, password });
 		} catch (error) {
 			console.trace(error);
 		}
