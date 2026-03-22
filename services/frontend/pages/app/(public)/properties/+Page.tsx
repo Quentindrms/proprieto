b0d9fd5b1c8648d7644de89ef2c41afe6095658f
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-22 12:15:07 +0100

    Feats(property): Add CreatePropertyForm component and integrate modal for property creation

--- services/frontend/pages/app/(public)/properties/+Page.tsx
+++ services/frontend/pages/app/(public)/properties/+Page.tsx
@@ -2,10 +2,16 @@ import type { Property } from "@app/types/property";
 import { Button } from "@components/button";
 import { StatCard, StatCardWrapper } from "@components/cards";
 import Heading from "@components/heading";
+import { Modal, ModalBody, ModalHeader } from "@components/modal";
 import PropertyCard from "@components/propertyCard";
 import SearchField from "@components/searchField";
+import Text from "@components/text";
+import { useModal } from "@hooks/useModal";
+import CreatePropertyForm from "./form";
 
 export default function Page() {
+	const modal = useModal(1500);
+
 	const property: Property = {
 		name: "Ma propriété",
 		purshacePrice: "100 000",
@@ -20,10 +26,24 @@ export default function Page() {
 	return (
 		<div class="w-dvw">
 			<div class="flex justify-between p-2">
+				<Modal
+					close={modal.close}
+					isClosing={modal.isClosing}
+					isOpened={modal.isOpened}
+				>
+					<ModalHeader>
+						<Heading components="h1" size="medium">
+							Ajouter une propriété
+						</Heading>
+					</ModalHeader>
+					<ModalBody>
+						<CreatePropertyForm />
+					</ModalBody>
+				</Modal>
 				<Heading components="h1" size="large" color="white">
 					Mes propriétés
 				</Heading>
-				<Button type="button" icons="add">
+				<Button type="button" icons="add" onClick={modal.open}>
 					Créer une nouvelle propriété
 				</Button>
 			</div>
---
+++ services/frontend/pages/app/(public)/properties/form.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@components/button";
+import { Form, Select, TextField } from "@components/form";
+import { useProperty } from "@hooks/useProperty";
+
+export default function CreatePropertyForm() {
+	const property = useProperty();
+
+	return (
+		<Form callback={property.create}>
+			<TextField
+				label="Nom"
+				type="text"
+				name="name"
+				onInput={property.handleCreateInput("name")}
+			/>
+			<TextField
+				label="Prix d'acquisition"
+				type="text"
+				name="purshacePrice"
+				onInput={property.handleCreateInput("purshacePrice")}
+			/>
+			<TextField
+				label="Date d'acquisition"
+				type="date"
+				name="purshaceDate"
+				onInput={property.handleCreateInput("purshaceDate")}
+			/>
+			<Select
+				label="Type de bien"
+				labelOptions="Sélectionner un type de bien"
+				options={[]}
+			/>
+			<div class="flex justify-center p-2">
+				<Button type="submit">Créer une nouvelle propriété</Button>
+			</div>
+		</Form>
+	);
+}
