7795dd953fb445a05f3f27c857e6586c12648de0
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-22 12:14:52 +0100

    Feats(property): Implement useProperty hook for managing property creation state

---
+++ services/frontend/hooks/useProperty.ts
@@ -0,0 +1,29 @@
+import type { CreateProperty } from "@app/types/property";
+import { createSignal } from "solid-js";
+
+export function useProperty() {
+	const [createProperty, setCreateProperty] = createSignal<CreateProperty>({
+		name: "",
+		isActive: false,
+		type: "",
+	});
+
+	function handleCreateInput(field: keyof CreateProperty) {
+		return (event: InputEvent) => {
+			const target = event.target as HTMLInputElement;
+			setCreateProperty((prev) => ({
+				...prev,
+				[field]: target.value,
+			}));
+			console.log(createProperty());
+		};
+	}
+
+	function create() {}
+
+	return {
+		createProperty,
+		handleCreateInput,
+		create,
+	};
+}
