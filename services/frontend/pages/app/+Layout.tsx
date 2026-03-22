34dfe40b9167f755948ad00301f363498234048f
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-22 11:42:05 +0100

    Feats(layout): Add Layout component for consistent page structure

---
+++ services/frontend/pages/app/+Layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@components/navbar";
+import type { JSX } from "solid-js";
+import { Toaster } from "solid-toast";
+
+export default function Layout(props: { children?: JSX.Element }) {
+	return (
+		<div class="min-h-dvh bg-background-base flex">
+			<Toaster position="top-right" />
+			<Navbar />
+			<main class="flex min-w-0">{props.children}</main>
+		</div>
+	);
+}
