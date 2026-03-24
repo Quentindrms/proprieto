b8294ae298a2bb1b8327595439c520ad533e52f2
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-24 15:15:09 +0100

    Feats(property): Implement createProperty method in PropertyController for user property creation

--- services/backend/src/property/property.controller.ts
+++ services/backend/src/property/property.controller.ts
@@ -1,8 +1,23 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post, Req, Res } from "@nestjs/common";
+import type { Request, Response } from "express";
+import type { CreatePropertyDto } from "types/DtoType";
 //biome-ignore lint/style/useImportType: required for NestJS DI
 import { PropertyService } from "./property.service";
 
 @Controller("property")
 export class PropertyController {
 	constructor(private readonly propertyService: PropertyService) {}
+
+	@Post("")
+	createProperty(
+		@Req() request: Request,
+		@Res() response: Response,
+		@Body() body: CreatePropertyDto,
+	) {
+		const user = request.user;
+		console.log(user);
+		if (!user) return response.status(401).send({});
+		console.log(user.email);
+		return response.status(200).send({ message: user.address });
+	}
 }
