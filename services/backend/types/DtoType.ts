14038ba335b5961a27973d708cf241aa6e47e873
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-24 14:52:12 +0100

    Refactor CreateAccountDto to use interface and add CreatePropertyDto for property creation

--- services/backend/types/DtoType.ts
+++ services/backend/types/DtoType.ts
@@ -1,8 +1,14 @@
-export type CreateAccountDto = {
+export interface CreateAccountDto {
 	name: string;
 	firstName: string;
 	address: string;
 	email: string;
 	phone: string;
 	password: string;
-};
+}
+
+export interface CreatePropertyDto {
+	name: string;
+	isActive: boolean;
+	type: string;
+}
