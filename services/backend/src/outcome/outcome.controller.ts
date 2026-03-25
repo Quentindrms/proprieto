b42b97138a3346e627cb633f9f7625642d09b708
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-25 19:44:04 +0100

    feat(outcome): implement createOutcome method in OutcomeService and add create endpoint in OutcomeController

--- services/backend/src/outcome/outcome.controller.ts
+++ services/backend/src/outcome/outcome.controller.ts
@@ -1,8 +1,22 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post, Req, Res } from "@nestjs/common";
+import type { Request, Response } from "express";
+import type { CreateOutcomeDto } from "types/DtoType";
 //biome-ignore lint/style/useImportType: required for NestJS DI
 import { OutcomeService } from "./outcome.service";
 
 @Controller("outcome")
 export class OutcomeController {
 	constructor(private readonly outcomeService: OutcomeService) {}
+
+	@Post("")
+	async create(
+		@Req() request: Request,
+		@Res() response: Response,
+		@Body() body: CreateOutcomeDto,
+	) {
+		const user = request.user;
+		if (!user) return response.status(401).send({});
+		console.log(body);
+		return response.status(200).send({ message: "success" });
+	}
 }
--- services/backend/src/outcome/outcome.service.ts
+++ services/backend/src/outcome/outcome.service.ts
@@ -1,4 +1,20 @@
+import { prisma } from "@libs/DatabaseClient";
 import { Injectable } from "@nestjs/common";
+import type { CreateOutcomeDto } from "@types/DtoType";
 
 @Injectable()
-export class OutcomeService {}
+export class OutcomeService {
+	async createOutcome(outcome: CreateOutcomeDto) {
+		return await prisma.outcome.create({
+			data: {
+				name: outcome.name,
+				amount: outcome.amount,
+				isReccuring: outcome.isRecuring,
+				isPaid: outcome.isPaid,
+				issueDate: new Date(outcome.issueDate),
+				paidOn: outcome.paidOn ? new Date(outcome.paidOn) : null,
+				/** TODO : implémenter propertyId, outcomeCategoryId, providersId */
+			},
+		});
+	}
+}
