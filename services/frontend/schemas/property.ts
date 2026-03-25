1e4db06523d19972bd5f99d572c4ff64cf8d2ac1
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-25 10:53:33 +0100

    Feats(property): update schemas to use coercion for purchasePrice and purchaseDate, and make sellPrice and sellDate optional

--- services/frontend/schemas/property.ts
+++ services/frontend/schemas/property.ts
@@ -4,10 +4,16 @@ export const nameSchema = z
 	.string()
 	.min(10, "Le nom d'une propriété doit contenir au moins 10 caractères")
 	.max(150, "Le nom d'une propriété ne peut exécéder 150 caractères");
-export const purchasePriceSchema = z.int("Le prix doit être un nombre");
-export const purchaseDateSchema = z.date("Le format de la date est invalide");
-export const sellpriceSchema = z.int("Le prix doit être un nombre");
-export const sellDateSchema = z.date("Le format de la date est invalide");
+export const purchasePriceSchema = z.coerce.number(
+	"Le prix doit être un nombre",
+);
+export const purchaseDateSchema = z.coerce.date(
+	"Le format de la date est invalide",
+);
+export const sellpriceSchema = z.int("Le prix doit être un nombre").optional();
+export const sellDateSchema = z
+	.date("Le format de la date est invalide")
+	.optional();
 
 export const PropertyCreationSchema = z.object({
 	name: nameSchema,
