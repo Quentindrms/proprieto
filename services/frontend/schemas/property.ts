c04e799becb80d674ccd6e789b65785dd26e50ec
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-25 09:48:18 +0100

    Feats(property): add validation schemas for property creation and fix typos in Property interface

---
+++ services/frontend/schemas/property.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const nameSchema = z
+	.string()
+	.min(10, "Le nom d'une propriété doit contenir au moins 10 caractères")
+	.max(150, "Le nom d'une propriété ne peut exécéder 150 caractères");
+export const purchasePriceSchema = z.int("Le prix doit être un nombre");
+export const purchaseDateSchema = z.date("Le format de la date est invalide");
+export const sellpriceSchema = z.int("Le prix doit être un nombre");
+export const sellDateSchema = z.date("Le format de la date est invalide");
+
+export const PropertyCreationSchema = z.object({
+	name: nameSchema,
+	purshasePrice: purchasePriceSchema,
+	purchaseDate: purchaseDateSchema,
+	sellPrice: sellpriceSchema,
+	sellDateSchema: sellDateSchema,
+});
+
+export type PropertyCreationType = z.infer<typeof PropertyCreationSchema>;
--- services/frontend/types/property.ts
+++ services/frontend/types/property.ts
@@ -2,18 +2,10 @@ export interface Property {
 	id: string;
 	name: string;
 	purchasePrice?: string;
-	purshaseDate?: Date;
+	purchaseDate?: Date;
 	sellPrice?: string;
 	sellDate?: Date;
 	isDeleted: boolean;
-	isActive: boolean;
-	type: string;
-}
-
-export interface CreateProperty {
-	name: string;
-	purshacePrice?: string;
-	purshaceDate?: Date;
 	isActive: boolean;
 	type: string;
 }
