5f77b77a54652fe3eb7fe138036b10fb866231d2
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-26 15:05:14 +0100

    feat(outcome): add categoryId to create and update outcome signals

--- services/frontend/hooks/useOutcome.ts
+++ services/frontend/hooks/useOutcome.ts
@@ -20,6 +20,7 @@ export function useOutcome() {
 		paidOn: new Date(),
 		frequency: "month",
 		propertyId: "",
+		categoryId: "",
 	});
 
 	const [updateOutcome, setUpdateOutcome] = createSignal<OutcomeUpdateType>({
@@ -32,6 +33,7 @@ export function useOutcome() {
 		paidOn: new Date(),
 		frequency: "month",
 		propertyId: "",
+		categoryId: "",
 	});
 
 	const [formError, setFormError] =
