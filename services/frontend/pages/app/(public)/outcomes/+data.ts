c6442590e2600f0115a24dae7455e8e0cd589ded
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-26 13:43:06 +0100

    feat(outcome): add data fetching for properties in outcomes page

---
+++ services/frontend/pages/app/(public)/outcomes/+data.ts
@@ -0,0 +1,14 @@
+import { PropertyService } from "@services/property.service";
+import { getCookiesFromPageContext } from "@utils/cookie";
+import type { PageContextServer } from "vike/types";
+
+export type Data = Awaited<ReturnType<typeof data>>;
+
+export async function data(pageContext: PageContextServer) {
+	const cookies = getCookiesFromPageContext(pageContext);
+
+	const propertyService = new PropertyService(cookies.auth);
+	const properties = await propertyService.browseProperties();
+	console.log(properties);
+	return { properties };
+}
