b1d7c7f65e9f85230268a6acb6e7fe6deb2c6ae9
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-26 15:39:01 +0100

    feat(provider): add schemas for creating and updating providers

---
+++ services/frontend/schemas/provider.ts
@@ -0,0 +1,48 @@
+import { email, z } from "zod";
+
+const idSchema = z.string("Identifiant créancier invalide");
+
+const nameSchema = z
+	.string()
+	.min(5, "Le nom doit contenir au moins 5 caractères")
+	.max(120, "Le nom ne peut pas contenir plus de 120 caractères");
+
+const firstNameSchema = z
+	.string()
+	.min(2, "Le prénom doit contenir au moins 2 caractères")
+	.max(120, "Le prénom ne peut pas contenir plus de 120 caractères");
+
+const emailSchema = z
+	.email("Le format de l'adresse email est invalide")
+	.max(254, "L'adresse email ne peut pas contenir plus de 120 caractères");
+
+const adressSchema = z
+	.string()
+	.min(5, "L'adresse doit contenir au moins 5 caracères")
+	.max(250, "L'adresse ne peut pas contenir plus de 250 caractères");
+
+const phoneSchema = z
+	.string()
+	.max(20, "Le téléphone ne peut pas contenir plus de 20 caractères");
+
+const userIdSchema = z.string("Identifiant utilisateur invalide");
+
+export const CreateProviderSchema = z.object({
+	name: nameSchema,
+	firstName: firstNameSchema,
+	email: emailSchema,
+	address: adressSchema,
+	phone: phoneSchema,
+});
+
+export const UpdateProviderSchema = z.object({
+	id: idSchema,
+	name: nameSchema,
+	firstName: firstNameSchema,
+	email: emailSchema,
+	address: adressSchema,
+	phone: phoneSchema,
+});
+
+export type CreateProviderType = z.infer<typeof CreateProviderSchema>;
+export type UpdateProviderType = z.infer<typeof UpdateProviderSchema>;
