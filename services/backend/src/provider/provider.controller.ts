c4a3e54c471c1e257d9e76a27eab93cc2abc395b
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-26 16:49:14 +0100

    feat(provider): implement ProviderController and ProviderService for provider creation with authentication

--- services/backend/src/provider/provider.controller.ts
+++ services/backend/src/provider/provider.controller.ts
@@ -1,8 +1,23 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post, Req, Res } from "@nestjs/common";
+import type { Request, Response } from "express";
+import type { CreateProviderDto } from "types/DtoType";
 //biome-ignore lint/style/useImportType: required for NestJS DI
 import { ProviderService } from "./provider.service";
 
 @Controller("provider")
 export class ProviderController {
 	constructor(private readonly providerService: ProviderService) {}
+
+	@Post("create")
+	async createProvider(
+		@Req() request: Request,
+		@Res() response: Response,
+		@Body() body: CreateProviderDto,
+	) {
+		const user = request.user;
+		if (!user) return response.status(401).send({});
+		const provider = await this.providerService.create(body, user.id);
+		if (!provider) return response.status(422).send({ message: "error" });
+		return response.status(201).send({ message: "success" });
+	}
 }
--- services/backend/src/provider/provider.service.ts
+++ services/backend/src/provider/provider.service.ts
@@ -1,4 +1,19 @@
+import { prisma } from "@libs/DatabaseClient";
 import { Injectable } from "@nestjs/common";
+import type { CreateProviderDto } from "types/DtoType";
 
 @Injectable()
-export class ProviderService {}
+export class ProviderService {
+	async create(provider: CreateProviderDto, userId: string) {
+		return await prisma.providers.create({
+			data: {
+				name: provider.name,
+				firstName: provider.firstName,
+				address: provider.address,
+				email: provider.email,
+				phone: provider.phone,
+				userId,
+			},
+		});
+	}
+}
