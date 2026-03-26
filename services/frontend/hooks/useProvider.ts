778d5ddfd7bf4cb9bba0713c25d39c51f9b03768
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-26 16:35:15 +0100

    feat(provider): add useProvider hook for managing provider creation and updates

---
+++ services/frontend/hooks/useProvider.ts
@@ -0,0 +1,68 @@
+import {
+	CreateProviderSchema,
+	type CreateProviderType,
+	type UpdateProviderType,
+} from "@schemas/provider";
+import { createSignal } from "solid-js";
+import type { ZodSafeParseError } from "zod";
+
+export function useProvider() {
+	const [createProvider, setCreateProvider] = createSignal<CreateProviderType>({
+		name: "",
+		firstName: "",
+		address: "",
+		email: "",
+		phone: "",
+		userId: "",
+	});
+
+	const [updateProvider, setUpdateProvider] = createSignal<UpdateProviderType>({
+		name: "",
+		firstName: "",
+		address: "",
+		email: "",
+		phone: "",
+		userId: "",
+		id: "",
+	});
+
+	const [formError, setFormError] =
+		createSignal<ZodSafeParseError<CreateProviderType | UpdateProviderType>>();
+
+	function handleCreateInput(field: keyof CreateProviderType) {
+		return (event: InputEvent) => {
+			const target = event.target as HTMLInputElement;
+			setCreateProvider((prev) => ({
+				...prev,
+				[field]: target.value,
+			}));
+		};
+	}
+
+	function handleUpdateInput(field: keyof UpdateProviderType) {
+		return (event: InputEvent) => {
+			const target = event.target as HTMLInputElement;
+			setUpdateProvider((prev) => ({
+				...prev,
+				[field]: target.value,
+			}));
+		};
+	}
+
+	function create() {
+		console.log(createProvider());
+		const validate = CreateProviderSchema.safeParse(createProvider());
+		console.log(validate);
+		if (!validate.success) {
+			setFormError(validate);
+			return;
+		}
+	}
+
+	return {
+		create,
+		formError,
+		handleCreateInput,
+		handleUpdateInput,
+	};
+}
