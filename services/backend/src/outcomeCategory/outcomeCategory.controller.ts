87bb1f4c0f5a5df56263141e8c545a2555aef36e
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-26 14:32:50 +0100

    feat(outcome): add OutcomeCategoryController and OutcomeCategoryService for category management

---
+++ services/backend/src/outcomeCategory/outcomeCategory.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Req, Res } from "@nestjs/common";
+import type { Request, Response } from "express";
+import type { OutcomeCategoryService } from "./outcomeCategory.service";
+
+@Controller()
+export class OutcomeCategoryController {
+	constructor(
+		private readonly outcomeCategoryService: OutcomeCategoryService,
+	) {}
+
+	@Get("/")
+	async browseCategory(@Req() request: Request, @Res() response: Response) {
+		const user = request.user;
+		if (!user) return response.status(401).send({});
+		const categories = await this.outcomeCategoryService.browseCategory();
+		return response.status(200).send({ categories });
+	}
+}
---
+++ services/backend/src/outcomeCategory/outcomeCategory.service.ts
@@ -0,0 +1,9 @@
+import { prisma } from "@libs/DatabaseClient";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class OutcomeCategoryService {
+	async browseCategory() {
+		return await prisma.outcomeCategory.findMany();
+	}
+}
