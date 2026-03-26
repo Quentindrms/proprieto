2211a5f73a02ce2ac0de589d0de18334f3ead44a
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-26 16:57:16 +0100

    feat(provider): implement data fetching for providers in +data.ts

---
+++ services/frontend/pages/app/(public)/contractors/+data.ts
@@ -0,0 +1,14 @@
+export type Data = Awaited<ReturnType<typeof data>>;
+
+import { ProviderService } from "@services/provider.service";
+import { getCookiesFromPageContext } from "@utils/cookie";
+import type { PageContextServer } from "vike/types";
+
+export async function data(pageContext: PageContextServer) {
+	const cookies = getCookiesFromPageContext(pageContext);
+
+	const providerService = new ProviderService(cookies.auth);
+	const providers = await providerService.browse();
+
+	return { providers };
+}
