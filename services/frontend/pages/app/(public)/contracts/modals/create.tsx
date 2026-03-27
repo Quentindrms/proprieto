a884ac7d528bda2e1ab1afc9716baa1b2156715f
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-27 15:46:52 +0100

    feat(contract): add CreateModal component for contract creation

---
+++ services/frontend/pages/app/(public)/contracts/modals/create.tsx
@@ -0,0 +1,29 @@
+import Heading from "@components/heading";
+import { Modal, ModalBody, ModalHeader } from "@components/modal";
+import type { Accessor } from "solid-js";
+import CreateForm from "../forms/create";
+
+interface CreateModalProps {
+	isOpened: Accessor<boolean>;
+	isClosing: Accessor<boolean>;
+	close: () => void;
+}
+
+export default function CreateModal(props: CreateModalProps) {
+	return (
+		<Modal
+			close={props.close}
+			isClosing={props.isClosing}
+			isOpened={props.isOpened}
+		>
+			<ModalHeader>
+				<Heading components="h3" size="medium">
+					Créer un contrat
+				</Heading>
+			</ModalHeader>
+			<ModalBody>
+				<CreateForm />
+			</ModalBody>
+		</Modal>
+	);
+}
