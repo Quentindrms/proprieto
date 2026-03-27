339363c4d36504d28975eef98cb1a12586a78b76
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-27 16:09:01 +0100

    feat(contract): add browse method to retrieve contracts from the server

--- services/frontend/services/contract.service.ts
+++ services/frontend/services/contract.service.ts
@@ -1,8 +1,13 @@
+import type { Contract } from "@app/types/contract";
 import type { CreateContractType } from "@schemas/contract";
 import { CoreService } from "./core.service";
 
 export class ContractService extends CoreService {
 	create(contract: CreateContractType) {
 		return this.post<{ message: string }>("/contract/", contract);
+	}
+
+	browse() {
+		return this.get<Contract[]>("/contract/browse");
 	}
 }
