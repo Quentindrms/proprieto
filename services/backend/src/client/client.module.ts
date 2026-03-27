7037a2bec86b7e752ac90c3feb5a08a3262ecfff
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-27 14:33:53 +0100

    feat(client): implement client creation logic and add authentication middleware

--- services/backend/src/client/client.module.ts
+++ services/backend/src/client/client.module.ts
@@ -1,4 +1,9 @@
-import { Module } from "@nestjs/common";
+import {
+	type MiddlewareConsumer,
+	Module,
+	type NestModule,
+} from "@nestjs/common";
+import { IsAuthenticatedMiddleware } from "middleware/is-authenticated.middleware";
 import { ClientController } from "./client.controller";
 import { ClientService } from "./client.service";
 
@@ -7,4 +12,8 @@ import { ClientService } from "./client.service";
 	controllers: [ClientController],
 	providers: [ClientService],
 })
-export class ClientModule {}
+export class ClientModule implements NestModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer.apply(IsAuthenticatedMiddleware).forRoutes(ClientController);
+	}
+}
