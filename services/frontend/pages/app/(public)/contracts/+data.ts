ada4f5b972331ac4a28d183e30744d89d97d6841
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-27 16:09:08 +0100

    feat(contract): implement data fetching for contracts, properties, and clients

---
+++ services/frontend/pages/app/(public)/contracts/+data.ts
@@ -0,0 +1,21 @@
+export type Data = Awaited<ReturnType<typeof data>>;
+
+import { ClientService } from "@services/client.service";
+import { ContractService } from "@services/contract.service";
+import { PropertyService } from "@services/property.service";
+import { getCookiesFromPageContext } from "@utils/cookie";
+import type { PageContextServer } from "vike/types";
+
+export async function data(pageContext: PageContextServer) {
+	const cookies = getCookiesFromPageContext(pageContext);
+
+	const contractService = new ContractService(cookies.auth);
+	const propertyService = new PropertyService(cookies.auth);
+	const clientService = new ClientService(cookies.auth);
+
+	const contract = contractService.browse();
+	const properties = propertyService.browseProperties();
+	const clients = clientService.browse();
+
+	return { contract, properties, clients };
+}
