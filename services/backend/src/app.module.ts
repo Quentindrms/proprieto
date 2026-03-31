403e4d62d273573a268fa0d197d5a9701afa2281
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-31 15:35:24 +0200

    feat(income): add IncomeCategoryModule to AppModule imports

--- services/backend/src/app.module.ts
+++ services/backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from "./auth/auth.module";
 import { ClientModule } from "./client/client.module";
 import { ContractModule } from "./contract/contract.module";
 import { IncomeModule } from "./income/income.module";
+import { IncomeCategoryModule } from "./incomeCategory/incomeCategory.module";
 import { OutcomeModule } from "./outcome/outcome.module";
 import { OutcomeCategoryModule } from "./outcomeCategory/outcomeCategory.module";
 import { PropertyModule } from "./property/property.module";
@@ -22,6 +23,7 @@ import { UserModule } from "./user/user.module";
 		ProviderModule,
 		UserModule,
 		OutcomeCategoryModule,
+		IncomeCategoryModule,
 	],
 	controllers: [AppController],
 	providers: [AppService],
