1309e6468300071e1b0f8eecde4c66d202e8a32b
Quentin Derimais <quentin.derimais@gmail.com>
2026-03-31 14:44:08 +0200

    feat(income): implement IncomeService and IncomeController for income creation

--- services/backend/src/income/income.controller.ts
+++ services/backend/src/income/income.controller.ts
@@ -1,8 +1,23 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post, Req, Res } from "@nestjs/common";
+import type { Request, Response } from "express";
+import type { CreateIncomeDto } from "types/DtoType";
 //biome-ignore lint/style/useImportType: required for NestJS DI
 import { IncomeService } from "./income.service";
 
 @Controller("income")
 export class IncomeController {
 	constructor(private readonly incomeService: IncomeService) {}
+
+	@Post("")
+	async create(
+		@Req() request: Request,
+		@Res() response: Response,
+		@Body() body: CreateIncomeDto,
+	) {
+		const user = request.user;
+		if (!user) return response.status(401).send({});
+		const income = this.incomeService.create(body);
+		if (!income) return response.status(404).send({ message: "error" });
+		return response.status(200).send({ message: "success" });
+	}
 }
