c3ed39a54cb2c566a63750e9c38b09bd3661573c
Quentin Derimais <quentin.derimais@gmail.com>
2026-04-03 14:31:19 +0200

    fix(contracts): correct console log to show properties

--- services/frontend/pages/app/(public)/contracts/+data.ts
+++ services/frontend/pages/app/(public)/contracts/+data.ts
@@ -17,7 +17,7 @@ export async function data(pageContext: PageContextServer) {
 	const properties = await propertyService.browseProperties();
 	const clients = await clientService.browse();
 
-	console.log(clients);
+	console.log(properties);
 
 	return { contracts, properties, clients };
 }
