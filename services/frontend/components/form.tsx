afda45cff38186948514269699dcb0870117307b
Quentin Derimais <quentin.derimais@gmail.com>
2026-04-04 13:51:33 +0200

    style(form): improve input field styling

--- services/frontend/components/form.tsx
+++ services/frontend/components/form.tsx
@@ -13,7 +13,7 @@ import Text from "./text";
 const FormContext = createContext<{
 	setHasRequiredFields: Setter<boolean>;
 }>({
-	setHasRequiredFields: () => () => {},
+	setHasRequiredFields: () => () => { },
 });
 
 interface FormProps extends JSX.FormHTMLAttributes<HTMLFormElement> {
@@ -86,7 +86,7 @@ export function TextField(props: TextFieldProps) {
 	return (
 		<fieldset class="flex flex-col">
 			<Label label={local.label} required={rest.required} for={rest.name} />
-			<input class="bg-primary rounded-md p-1" id={rest.name} {...rest} />
+			<input class="bg-background-base border-slate-strong border rounded-md p-1 shadow-md shadow-slate-marked" id={rest.name} {...rest} />
 		</fieldset>
 	);
 }
