78df25e9bb7c0cdc91682df00b27b7a290f41f12
Quentin Derimais <quentin.derimais@gmail.com>
2026-04-04 14:14:53 +0200

    style(auth): update login page styling and remove debug element

--- services/frontend/pages/auth/login/+Page.tsx
+++ services/frontend/pages/auth/login/+Page.tsx
@@ -4,10 +4,9 @@ import LoginForm from "./form";
 export default function Page() {
 	return (
 		<div class="flex flex-col justify-center items-center w-dvw">
-			<Heading components="h1" size="big" color="blue">
+			<Heading components="h1" size="big" color="black">
 				Connexion
 			</Heading>
-			<p class="text-slate-500">Prout</p>
 			<LoginForm />
 		</div>
 	);
