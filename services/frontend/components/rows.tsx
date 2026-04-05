324a6dbfd0b8897949e59f1536f6995108939ef7
Quentin Derimais <quentin.derimais@gmail.com>
2026-04-05 20:36:36 +0200

    feat(frontend): add income/expense indicators and dates to transaction rows

--- services/frontend/components/rows.tsx
+++ services/frontend/components/rows.tsx
@@ -1,4 +1,7 @@
-import { BsArrowUpRightCircleFill } from "solid-icons/bs";
+import {
+    BsArrowDownRightCircleFill,
+    BsArrowUpRightCircleFill,
+} from "solid-icons/bs";
 import Heading from "./heading";
 import Text from "./text";
 
@@ -7,19 +10,23 @@ interface TransactionRowProps {
     recipient: string;
     amount: string;
     income: boolean;
+    date: Date;
 }
 
 export default function TransactionRow(props: TransactionRowProps) {
     return (
-        <div class="flex gap-2 items-center p-2 bg-background-base w-md">
-            <BsArrowUpRightCircleFill class="m-2" size={35} color="var(--color-action-green)" />
+        <div class="flex gap-3 items-center p-2 bg-background-base w-md">
+            {props.income ? <BsArrowUpRightCircleFill class="m-2" size={35} color="var(--color-action-green)" /> : <BsArrowDownRightCircleFill class="m-2" size={35} color="var(--color-action-red)" />}
             <div class="text-left p-2">
                 <Heading components="h3" size="medium">
                     {props.name}
                 </Heading>
                 <Text size="large">{props.recipient}</Text>
             </div>
-            <Text size="large">{props.amount}€</Text>
+            <div class="p-1">
+                <Text size="large" >{props.income ? "+" : "-"}{props.amount}€</Text>
+                <Text size="small" >{new Date(props.date).toLocaleDateString("fr-FR")}</Text>
+            </div>
         </div>
     );
 }
