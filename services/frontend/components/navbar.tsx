25ee413ef5c64c36705b27381a654069651efc77
Quentin Derimais <quentin.derimais@gmail.com>
2026-04-06 11:57:59 +0200

    refactor(navbar): simplify and restyle navigation component

--- services/frontend/components/navbar.tsx
+++ services/frontend/components/navbar.tsx
@@ -4,30 +4,16 @@ import Text from "./text";
 export default function Navbar() {
 
     return (
-        <div class="h-dvh w-xs shrink-0 p-2 flex flex-col border-r border-background-border">
-            <div class="text-center p-3">
-                <Heading components="h1" size="big"><a href="/app">Proprieto</a></Heading>
-                <Text components="p">Gestion immobilière</Text>
+        <div class="w-2xs">
+            <div class="flex flex-col p-4">
+                <Heading components="h1" size="large" fontClasses="bold">Proprieto</Heading>
+                <Text size="small" class="text-muted-text font-base-regular">Gestionnaire de propriété</Text>
             </div>
-
-            <div class="flex flex-col gap-2">
-                <Heading components="h2" size="large" color="white">Vue générale</Heading>
-                <NavbarLink name="Tableau de bord" value="/app/" />
-
-                <Heading components="h2" size="large" color="white">Patrimoine</Heading>
-                <NavbarLink name="Propriétés" value="/app/properties" />
-                <NavbarLink name="Contrats" value="/app/contracts" />
-
-                <Heading components="h2" size="large" color="white">Finances</Heading>
-                <NavbarLink name="Revenus" value="/app/incomes" />
-                <NavbarLink name="Dépense" value="/app/outcomes" />
-
-                <Heading components="h2" size="large" color="white">Contacts</Heading>
-                <NavbarLink name="Clients" value="/app/clients" />
-                <NavbarLink name="Prestataires" value="/app/contractors" />
-
-                <Heading components="h2" size="large" color="white">Compte</Heading>
-                <NavbarLink name="Paramètres" value="/app/account" />
+            <div class="flex flex-col text-right">
+                <NavbarLink name="Portefeuille" value="" />
+                <NavbarLink name="Opérations" value="" />
+                <NavbarLink name="Finance" value="" />
+                <NavbarLink name="Paramètres" value="" />
             </div>
         </div>
     )
@@ -41,7 +27,7 @@ interface NavbarLinkProps {
 export function NavbarLink(props: NavbarLinkProps) {
 
     return (
-        <a href={props.value} class="text-primary hover:text-solid-gold-hover text-medium pl-2">
+        <a href={props.value} class="font-base-medium">
             {props.name}</a>
     )
 }
