653d6492114a3b9e4481d03fe5a570f2a010a760
Quentin Derimais <quentin.derimais@gmail.com>
2026-04-07 21:52:11 +0200

    refactor(heading): split props and fix class handling in Heading component

--- services/frontend/components/heading.tsx
+++ services/frontend/components/heading.tsx
@@ -1,17 +1,28 @@
 import clsx from "clsx";
-import type { JSX, JSXElement } from "solid-js";
+import { type JSX, type JSXElement, splitProps } from "solid-js";
 import { Dynamic } from "solid-js/web";
-import type { FontType, HeadingColor, HeadingComponents, TextSize } from "../types/styleTypes";
+import type {
+    FontType,
+    HeadingColor,
+    HeadingComponents,
+    TextSize,
+} from "../types/styleTypes";
 
 interface HeadingProps extends JSX.HTMLAttributes<HTMLHeadingElement> {
     size: TextSize;
     components: HeadingComponents;
     children: JSXElement;
-    color?: HeadingColor,
-    fontClasses?: FontType,
+    color?: HeadingColor;
+    fontClasses?: FontType;
 }
 
 export default function Heading(props: HeadingProps) {
+    const [rest, local] = splitProps(props, [
+        "size",
+        "components",
+        "color",
+        "fontClasses",
+    ]);
 
     const sizeClasses: Record<TextSize, string> = {
         "extra-small": "text-2xs",
@@ -21,7 +32,7 @@ export default function Heading(props: HeadingProps) {
         large: "text-xl",
         "extra-large": "text-2xl",
         big: "text-3xl",
-    }
+    };
 
     const colorCLasses: Record<HeadingColor, string> = {
         green: "text-action-green",
@@ -30,19 +41,19 @@ export default function Heading(props: HeadingProps) {
         black: "text-deep-neutral",
         white: "text-slate-500",
         gray: "text-muted-text",
-    }
+    };
 
     const fontClasses: Record<FontType, string> = {
         "extra-bold": "font-base-extrabold",
-        "bold": "font-base-bold",
-        "medium": "font-base-medium",
-        "regular": "font-base-regular",
-    }
+        bold: "font-base-bold",
+        medium: "font-base-medium",
+        regular: "font-base-regular",
+    };
 
-    const globalClasses = ""
+    const globalClasses = "";
 
     return (
-        <Dynamic component={props.components} class={clsx([globalClasses, sizeClasses[props.size], props.color ? colorCLasses[props.color] : colorCLasses.black, props.fontClasses ? fontClasses[props.fontClasses] : fontClasses["medium"]])}>
+        <Dynamic component={props.components} class={clsx(local.class, globalClasses, sizeClasses[props.size], props.color ? colorCLasses[props.color] : colorCLasses.black, props.fontClasses ? fontClasses[props.fontClasses] : fontClasses["medium"])}>
             {props.children}
         </Dynamic>
     );
