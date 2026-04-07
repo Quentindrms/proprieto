8074b3368cef3d8177d122fadcac17299082c05b
Quentin Derimais <quentin.derimais@gmail.com>
2026-04-07 11:42:23 +0200

    feat(landing): add navbar to layout component

--- services/frontend/pages/(landing)/+Layout.tsx
+++ services/frontend/pages/(landing)/+Layout.tsx
@@ -1,9 +1,13 @@
+import Navbar from "@components/navbar";
 import type { JSX } from "solid-js";
 
 export default function Layout(props: { children?: JSX.Element }) {
     return (
         <div class="bg-background-muted/10 h-full">
-            {props.children}
+            <div class="flex">
+                <Navbar />
+                {props.children}
+            </div>
         </div>
     )
 }
