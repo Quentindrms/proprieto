07e0d5c728cbddeec3db43e55508498706511660
Quentin Derimais <quentin.derimais@gmail.com>
2026-04-08 22:46:01 +0200

    fix(ui): improve contract expiration card layout

--- services/frontend/components/contract.tsx
+++ services/frontend/components/contract.tsx
@@ -10,7 +10,7 @@ interface ContractExpireSoonProps {
 export default function ContractExpireSoon(props: ContractExpireSoonProps) {
 
     return (
-        <div class="flex flex-row items-center bg-background-base rounded-xl">
+        <div class="flex flex-row items-center justify-between bg-background-base rounded-xl">
             <div class="flex flex-col">
                 <Heading components="h2" size="medium">{props.clientName} - {props.contractName}</Heading>
                 <Text class="">Expire le {props.expireDate.toLocaleDateString("fr-FR")}</Text>
