342691ba906c68ba5b0f924cc912203de049a0d4
Quentin Derimais <quentin.derimais@gmail.com>
2026-04-08 09:59:13 +0200

    feat(ui): add clickable button badge component

--- services/frontend/components/badge.tsx
+++ services/frontend/components/badge.tsx
@@ -1,10 +1,11 @@
 import clsx from "clsx";
-import type { JSX } from "solid-js";
-import type { BadgeColor } from "../types/styleTypes";
+import { type JSX, splitProps } from "solid-js";
+import type { BadgeColor, ButtonColor } from "../types/styleTypes";
 
 interface BadgeProps {
     color: BadgeColor;
     children: JSX.Element;
+    onClick: () => void;
 }
 
 export function Badge(props: BadgeProps) {
@@ -12,18 +13,43 @@ export function Badge(props: BadgeProps) {
         success: "bg-action-green text-green-800 font-base-extrabold",
         error: "bg-action-red text-dark text-red-800 font-base-extrabold",
         primary: "bg-background-primary text-light font-base-extrabold",
-        warning: "bg-action-orange text-amber-800 font-base-extrabold"
+        warning: "bg-action-orange text-amber-800 font-base-extrabold",
     };
 
-    const globalClasses = "w-fit h-fit pl-4 pr-4 pb-2 pt-2 rounded-full shadow-xs shadow-background-muted";
+    const globalClasses =
+        "w-fit h-fit pl-4 pr-4 pb-2 pt-2 rounded-full shadow-xs shadow-background-muted";
 
     return (
-        <div
-            class={clsx([
-                globalClasses,
-                colorClases[props.color
-                ]])}
-        >
+        <div class={clsx([globalClasses, colorClases[props.color]])}>
             {props.children}
-        </div>);
+        </div>
+    );
+}
+
+interface ButtonBadgeProps extends JSX.ButtonHTMLAttributes<HTMLButtonElement> {
+    color: BadgeColor;
+    children: JSX.Element;
+    onClick: () => void;
+}
+
+export function ButtonBadge(props: ButtonBadgeProps) {
+    const [local, rest] = splitProps(props, ["color", "children"]);
+
+    const colorClases: Record<BadgeColor, string> = {
+        success: "bg-action-green text-green-800 font-base-extrabold",
+        error: "bg-action-red text-dark text-red-800 font-base-extrabold",
+        primary: "bg-background-primary text-light font-base-extrabold",
+        warning: "bg-action-orange text-amber-800 font-base-extrabold",
+    };
+
+    const globalClasses =
+        "w-fit h-fit pl-4 pr-4 pb-2 pt-2 rounded-full shadow-xs shadow-background-muted";
+
+    return (
+        <div class={clsx([globalClasses, colorClases[props.color]])}>
+            {props.children}
+        </div>
+    );
+
+
 }
