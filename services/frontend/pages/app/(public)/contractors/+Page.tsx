8a5c11e95e4a7de18244e7d5af58a578183cc8d0
Quentin Derimais <quentin.derimais@gmail.com>
2026-04-09 14:39:16 +0200

    refactor(pages): simplify page layout and remove unused imports

--- services/frontend/pages/app/(public)/contractors/+Page.tsx
+++ services/frontend/pages/app/(public)/contractors/+Page.tsx
@@ -1,7 +1,5 @@
-import Board from "@components/board";
-import { StatCard, StatCardWrapper } from "@components/cards";
+import { ButtonGroup } from "@components/button";
 import PageNamer from "@components/pageNamer";
-import Text from "@components/text";
 import { useModal } from "@hooks/useModal";
 import { useData } from "vike-solid/useData";
 import type { Data } from "./+data";
@@ -20,11 +18,12 @@ export default function Page() {
 	]);
 
 	return (
-		<div class="w-full h-dvh">
+		<div class="w-full">
 			<PageNamer
 				pageName="Mes prestataires"
 				buttonText="Ajouter un prestataire"
 				onClick={() => createModal.open()}
+				subText="Gérez votre réseau de partenaires pour les interventions techniques"
 			/>
 
 			<CreateModal
@@ -33,25 +32,12 @@ export default function Page() {
 				isOpened={createModal.isOpened}
 			/>
 
-			<StatCardWrapper>
-				<StatCard
-					legend="Clients totaux"
-					value={String(data.providers.length)}
-					title=""
-				/>
-
-				<StatCard legend="Avec contrat actif" value="0" title="" />
-
-				<StatCard legend="Sans contrat" value="0" title="" />
-			</StatCardWrapper>
-
-			<div class="p-5 grid grid-cols-[max-content_max-content_max-content] gap-x-5 gap-y-5">
-				<Board
-					name="Pestataires"
-					columns={["Nom", "Prénom", "Email", "Adresse", "Téléphone"]}
-					cells={cells}
-				></Board>
-			</div>
+			<ButtonGroup options={[
+				{ label: "Tous", value: "all" },
+				{ label: "Plomberie", value: "plumber", },
+				{ label: "Chauffage", value: "chauffage" },
+				{ label: "Électricité", value: "electricity" }
+			]} />
 		</div>
 	);
 }
