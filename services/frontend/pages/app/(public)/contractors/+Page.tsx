812a73ea5fd9359f53d4fd8a08be0d51f5a612b1
Quentin Derimais <quentin.derimais@gmail.com>
2026-04-09 15:09:06 +0200

    refactor(page): simplify contractor data extraction

--- services/frontend/pages/app/(public)/contractors/+Page.tsx
+++ services/frontend/pages/app/(public)/contractors/+Page.tsx
@@ -1,5 +1,7 @@
+import { ContractorsBoard } from "@components/board";
 import { ButtonGroup } from "@components/button";
 import PageNamer from "@components/pageNamer";
+import type { ContractorRowData } from "@components/rows";
 import { useModal } from "@hooks/useModal";
 import { useData } from "vike-solid/useData";
 import type { Data } from "./+data";
@@ -9,16 +11,17 @@ export default function Page() {
 	const createModal = useModal(350);
 	const data = useData<Data>();
 
-	const cells = data.providers.map((provider) => [
-		provider.directories.name,
-		provider.directories.firstName,
-		provider.directories.email,
-		provider.directories.address,
-		provider.directories.phone,
-	]);
+	const contractor: ContractorRowData[] = data.providers.map((provider) => ({
+		name: `${provider.directories.firstName} ${provider.directories.name}`,
+		speciality: "spécialité",
+		phone: provider.directories.phone,
+		mail: provider.directories.email,
+	}));
+
+	console.log(contractor);
 
 	return (
-		<div class="w-full">
+		<div class="w-full flex flex-col gap-5">
 			<PageNamer
 				pageName="Mes prestataires"
 				buttonText="Ajouter un prestataire"
@@ -32,12 +35,16 @@ export default function Page() {
 				isOpened={createModal.isOpened}
 			/>
 
-			<ButtonGroup options={[
-				{ label: "Tous", value: "all" },
-				{ label: "Plomberie", value: "plumber", },
-				{ label: "Chauffage", value: "chauffage" },
-				{ label: "Électricité", value: "electricity" }
-			]} />
+			<ButtonGroup
+				options={[
+					{ label: "Tous", value: "all" },
+					{ label: "Plomberie", value: "plumber" },
+					{ label: "Chauffage", value: "chauffage" },
+					{ label: "Électricité", value: "electricity" },
+				]}
+			/>
+
+			<ContractorsBoard contractors={contractor} />
 		</div>
 	);
 }
