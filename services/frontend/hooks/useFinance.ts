39159cc5f99b7c802971d0af4a3cb3119399c6cd
Quentin Derimais <quentin.derimais@gmail.com>
2026-04-10 15:29:25 +0200

    feat(finance): add finance management hooks

---
+++ services/frontend/hooks/useFinance.ts
@@ -0,0 +1,72 @@
+import type { IncomeCreationType } from "@schemas/income";
+import type { OutcomeCreationType } from "@schemas/outcome";
+import { createSignal } from "solid-js";
+import type { ZodSafeParseError } from "zod";
+
+export function useFinance() {
+	const [createIncome, setCreateIncome] = createSignal<IncomeCreationType>({
+		name: "",
+		amount: 0,
+		contractId: "",
+		incomeCategoryId: "",
+		isPaid: false,
+		frequency: "none",
+		paidOn: undefined,
+		issueDate: new Date(),
+	});
+
+	const [createOutcome, setCreateOutcome] = createSignal<OutcomeCreationType>({
+		name: "",
+		amount: 0,
+		categoryId: "",
+		isPaid: false,
+		isRecurring: false,
+		propertyId: "",
+		providerId: "",
+		frequency: "none",
+		issueDate: undefined,
+		paidOn: undefined,
+	});
+
+	const [outcomeErrors, setOutcomeErrors] =
+		createSignal<ZodSafeParseError<OutcomeCreationType>>();
+	const [incomeErrors, setIncomeErrors] =
+		createSignal<ZodSafeParseError<IncomeCreationType>>();
+
+	function handleInputIncome(field: keyof IncomeCreationType) {
+		return (event: InputEvent) => {
+			const target = event.target as HTMLInputElement;
+			setCreateIncome((prev) => ({
+				...prev,
+				[field]: target.checked ? target.checked : target.value,
+			}));
+		};
+	}
+
+	function handleInputOutcome(field: keyof OutcomeCreationType) {
+		return (event: InputEvent) => {
+			const target = event.target as HTMLInputElement;
+			setCreateOutcome((prev) => ({
+				...prev,
+				[field]: target.checked ? target.checked : target.value,
+			}));
+		};
+	}
+
+	function handleCreateIncome() {
+		console.log(createIncome());
+	}
+
+	function handleCreateOutcome() {
+		console.log(createOutcome);
+	}
+
+	return {
+		handleInputIncome,
+		handleInputOutcome,
+		handleCreateIncome,
+		handleCreateOutcome,
+		outcomeErrors,
+		incomeErrors,
+	};
+}
