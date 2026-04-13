aa748f7463c21c9ee13da912984f49690f38ee12
Quentin Derimais <quentin.derimais@gmail.com>
2026-04-13 14:21:15 +0200

    feat(finances): enhance details modal with structured finance information

--- services/frontend/pages/app/(public)/finances/modals/details.tsx
+++ services/frontend/pages/app/(public)/finances/modals/details.tsx
@@ -1,6 +1,8 @@
 import type { FluxBoardItem } from "@components/board";
+import { ActionButton } from "@components/button";
 import Heading from "@components/heading";
 import { Modal, ModalBody, ModalHeader } from "@components/modal";
+import Text from "@components/text";
 import type { Accessor } from "solid-js";
 
 interface DetailsModal {
@@ -19,11 +21,19 @@ export default function DetailsModal(props: DetailsModal) {
         >
             <ModalHeader>
                 <Heading class="" components="h2" size="medium" fontClasses="bold">
-                    Coucou le monde
+                    Détails de votre {props.finances.type === "income" && "revenu" || props.finances.type === "outcome" && "dépense"} du {props.finances.issueDate}
                 </Heading>
             </ModalHeader>
             <ModalBody>
-                <p>Hello world {props.finances.name}</p>
+                <Text>Intitulé : {props.finances.name}</Text>
+                <Text>Montant : {props.finances.amount} €</Text>
+                <Text>Catégorie : {props.finances.category}</Text>
+                <Text>Date de mise en recouvrement : {props.finances.issueDate}</Text>
+                <Text>Statut : !!!mettre le statut!!!</Text>
+                <div class="flex gap-2 justify-between">
+                    <ActionButton>Modifier</ActionButton>
+                    <ActionButton>Supprimer</ActionButton>
+                </div>
             </ModalBody>
         </Modal>
     );
