d0a33e6fae955983e735e3347e50ae1517121af4
Quentin Derimais <quentin.derimais@gmail.com>
2026-04-23 11:42:34 +0200

    feat(types): add UpdateOutcomeDto and isDeleted field to CreateOutcomeDto

--- services/backend/types/DtoType.ts
+++ services/backend/types/DtoType.ts
@@ -36,6 +36,22 @@ export interface CreateOutcomeDto {
 	providerId: string;
 	propertyId: string;
 	categoryId: string;
+	isDeleted: boolean;
+}
+
+export interface UpdateOutcomeDto {
+	id: string;
+	name: string;
+	amount: number;
+	isRecurring: boolean;
+	isPaid: boolean;
+	issueDate: Date;
+	paidOn?: Date;
+	frequency: string;
+	providerId: string;
+	propertyId: string;
+	categoryId: string;
+	isDeleted: boolean;
 }
 
 export interface CreateProviderDto {
@@ -100,18 +116,4 @@ export interface UpdateIncomeDto {
 	frequency: string;
 	categoryId: string;
 	contractId: string;
-}
-
-export interface UpdateOutcomeDto {
-	id: string;
-	name: string;
-	amount: number;
-	isRecurring: boolean;
-	isPaid: boolean;
-	issueDate: Date;
-	paidOn?: Date;
-	frequency: string;
-	providerId: string;
-	propertyId: string;
-	categoryId: string;
 }
