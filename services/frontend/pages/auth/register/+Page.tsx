f4e6e26e517628970209e3dab1ff3f259a2187d0
Quentin Derimais <quentin.derimais@gmail.com>
2026-05-05 09:41:46 +0200

    feat(auth): add back navigation buttons to login and register pages

--- services/frontend/pages/auth/register/+Page.tsx
+++ services/frontend/pages/auth/register/+Page.tsx
@@ -1,10 +1,16 @@
+import { ActionButton } from "@components/button";
 import Heading from "@components/heading";
 import RegisterForm from "./form";
 
 export default function Page() {
 
     return (
-        <div class="h-dvh w-full flex flex-col justify-center items-center gap-4">
+        <div class="relative h-dvh w-full flex flex-col justify-center items-center gap-4">
+            <div class="absolute top-6 left-6">
+                <a href="/auth/login">
+                    <ActionButton color="outline">← Connexion</ActionButton>
+                </a>
+            </div>
             <Heading components="h1" size="extra-large" fontClasses="extra-bold">Inscription</Heading>
             <RegisterForm />
         </div>
