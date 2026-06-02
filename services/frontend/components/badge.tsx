bde9bf294287d7b3866a2f44ab472c7ecb2c8acc
Quentin Derimais <quentin.derimais@gmail.com>
2026-06-02 20:07:41 +0200

    feat(frontend): add BadgeBoard component

--- services/frontend/components/badge.tsx
+++ services/frontend/components/badge.tsx
@@ -29,6 +29,28 @@ export function Badge(props: BadgeProps) {
     );
 }
 
+export function BadgeBoard(props: BadgeProps) {
+    const colorClases: Record<BadgeColor, string> = {
+        success:
+            "bg-action-green/65 border border-action-green text-green-900 font-base-extrabold backdrop-blur-md",
+        error:
+            "bg-action-red/65 border-action-red text-dark text-red-900 font-base-extrabold",
+        primary:
+            "bg-background-primary/65 border-background-primary text-light font-base-extrabold",
+        warning:
+            "bg-action-orange/65 text-orange-900 border border-action-orange font-base-extrabold",
+    };
+
+    const globalClasses =
+        "w-40 text-center h-fit pl-4 pr-4 pb-2 pt-2 rounded-full shadow-md hover:-translate-x-1 transition-transform";
+
+    return (
+        <div class={clsx([globalClasses, colorClases[props.color]])}>
+            {props.children}
+        </div>
+    );
+}
+
 interface ButtonBadgeProps extends JSX.ButtonHTMLAttributes<HTMLButtonElement> {
     color: BadgeColor;
     children: JSX.Element;
